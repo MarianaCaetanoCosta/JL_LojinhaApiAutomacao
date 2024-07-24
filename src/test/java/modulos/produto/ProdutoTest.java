a19d0efeeede48c3e062d957672ecbea8fe90770
Mariana Caetano <mariana.cae.costa@gmail.com>
2024-07-24 01:30:44 -0300

    #Aula 3 - Implementado @BeforeEach e testes individuais
    1. Implementado notação @BeforeEach - executa a validação do token antes dos testes
    2. Implementado testes individuais para validar valor igual a 0.00 e 700.01

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.internal.common.assertion.Assertion;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,36 +15,38 @@ import static org.hamcrest.Matchers.*;
 
 @DisplayName("Testes de API Reste do módulo de Produto")
 public class ProdutoTest {
-    @Test
-    @DisplayName("Validar os limites proibidos do valor do produto")
-    public void testValidarLimitesProibidosValorProduto(){
+
+    private String token;
+
+    //Configuração que precisa acontecer antes de executar teste
+    @BeforeEach
+    public void beforeEach(){
         //Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
         //port = 8080;
         basePath = "/lojinha";
 
         //Obter o token do usuário admin
-        String token =
-            given()
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "  \"usuarioLogin\": \"admin\",\n" +
-                        "  \"usuarioSenha\": \"admin\"\n" +
-                        "}")
-            .when()
+        this.token = given()
+                        .contentType(ContentType.JSON)
+                        .body("{\n" +
+                                "  \"usuarioLogin\": \"admin\",\n" +
+                                "  \"usuarioSenha\": \"admin\"\n" +
+                                "}")
+                .when()
                     .post("/v2/login")
-            .then()
+                .then()
                     .extract()
-                    .path("data.token");
-                    //.path("message");
+                        .path("data.token");
+    }
 
-                //System.out.println(token);
-
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 0.00 não é permitido")
+    public void testValidarLimitesZeradoProibidosValorProduto(){
         //Tentar inserir produto com valor 0.00, validar mensagem de erro e status code 422
-
         given()
                 .contentType(ContentType.JSON)
-                .header("token", token)
+                .header("token", this.token)
                 .body("{\n" +
                         "  \"produtoNome\": \"Playstation 5\",\n" +
                         "  \"produtoValor\": 0.00,\n" +
@@ -66,6 +69,39 @@ public class ProdutoTest {
                 .post("/v2/produtos")
         .then()
                 .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 7000.01 não é permitido")
+    public void testValidarLimitesMaiorSeteMilProibidosValorProduto(){
+        //Tentar inserir produto com valor 0.00, validar mensagem de erro e status code 422
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 7000.01,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\", \"rosa\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    },\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Jogo de Aventura\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+        .when()
+            .post("/v2/produtos")
+        .then()
+            .assertThat()
                 .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
                 .statusCode(422);
     }
